e14f13c9ffc9924b572464689dfbdb557e8c17c0
Paulo Henrique <ph46163835@gmail.com>
2022-09-23 14:19:21 -0300

    iniciando

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-// ...
+// ... bora 
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
