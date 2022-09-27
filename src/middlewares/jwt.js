bad10407f6f9020562d5f8324515a04fb8afdd6c
Paulo Henrique <ph46163835@gmail.com>
2022-09-26 22:04:21 -0300

    correção

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,23 +1,28 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
+
+
 const checkToken = (request, response, next) => {
-     const token = request.headers.authorization;
+    const token = request.headers['authorization']
     const SECRET = process.env.JWT_SECRET;
 
     if (!token) {
-        return response.status(401).json({ message: 'Token not found' });
+        return response.status(401).json({ message: "Token not found" })
     }
 
     try {
-        const user = jwt.verify(token, SECRET);
-         if (!user) {
-            return response.status(401).json({ message: 'Expired or invalid token' });
-         }
-        next();
-        } catch (error) {
-         return response.status(401).json({ message: 'Expired or invalid token' });
+        const user = jwt.verify(token, SECRET)
+        if (!user) {
+            return response.status(401).json({ message: "Expired or invalid token" })
+        }
+        next()
     }
-};
+    catch (error) {
+        return response.status(401).json({ message: "Expired or invalid token" })
+    }
+
+}
+
 
 module.exports = { checkToken };
